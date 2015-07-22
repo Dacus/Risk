4456ea6dc4b3eb50e049fb6f5d4e2b9160630e5f
FlorinBalint <fcsb_florin93@yahoo.com>
2015-07-22 11:59:10 +0300

    changed bonus to verify null reference instead of catching the exception

--- src/tora/train/risk/BonusStrategy.java
+++ src/tora/train/risk/BonusStrategy.java
@@ -1,8 +1,16 @@
 package tora.train.risk;
 
-/**
- * Created by intern on 7/20/15.
+/** Interface to be implemented for eventually computing the bonuses in the
+ * reinforcement phase
  */
 public interface BonusStrategy {
+    /**
+     * Compute the player bonus depending on owned continents & territories
+     * player == CPU || player == null returns 0
+     * player having no territory returns 0
+     *
+     * @param player the player to compute bonus for
+     * @return the reinforcements it is allowed to make this turn
+     */
     int computePlayerBonus(Player player);
 }
