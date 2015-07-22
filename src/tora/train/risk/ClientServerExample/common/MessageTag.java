209bd0a434fa1ebaec5236b03f48a23ff7ed0810
andreademe <andrea.deme>
2015-07-21 19:21:37 -0700

    Add clientserver package ClientServerExample update.  1. When a new client connects:  a.) The client sends its name to the server. The server gives the client an identification number  b.) Its name is broadcast to all other players i.e. the ComboBox is updated with the new client's name  c.) Online players appear in the new client's ComboBox  2. Each client may press ´Ready' once. When all connected clients press 'Ready', the game starts.  3. When a client disconnects, the ComboBox is updated again.
     Stopping the main server still not fixed.

--- src/tora/train/risk/ClientServerExample/common/MessageTag.java
+++
@@ -1,9 +0,0 @@
-package tora.train.risk.ClientServerExample.common;
-
-/**
- * Type of message
- */
-public enum MessageTag {
-	USER, GLOBAL, IDENTITY, READY, START, CONNECT, STOP, PLAYER_DISCONNECTED, NEW_PLAYER_CONNECTED,
-    ONLINE_PLAYERS
-}
