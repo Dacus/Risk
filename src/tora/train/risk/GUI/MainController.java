6326a7cc632d13981049ff13ac32188ce96daf83
isaLorincz <lorincz_isabela@yahoo.com>
2015-07-22 10:42:02 +0300

    gui: reinforcements are sent by pressing "Add reinforcement"

--- src/tora/train/risk/GUI/MainController.java
+++ src/tora/train/risk/GUI/MainController.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by intern on 7/20/15.
@@ -49,9 +48,11 @@ public class MainController {
     private class btnReinforcementAction implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            reinforcements.put(new Point(mainView.getXpos(), mainView.getYpos()), mainView.getValue());
+            Point pos=new Point(mainView.getXpos(), mainView.getYpos());
+            int value=mainView.getValue();
+            reinforcements.put(pos, value);
+            StaticInformations.submitReinforcements(pos, value);
             mainView.showLeftReinforcements();
-
         }
     }
 
@@ -59,9 +60,11 @@ public class MainController {
         @Override
         public void actionPerformed(ActionEvent e) {
             playersPlayed++;
+            /* use this if all commands are sent at the end
             for (Map.Entry<Point, Integer> pair : reinforcements.entrySet()) {
                 StaticInformations.submitReinforcements(pair.getKey(), pair.getValue());
             }
+            */
             System.out.println(reinforcements.toString());
             StaticInformations.endCurrentPlayerTurn();
             if (playersPlayed>StaticInformations.getPlayersNumber()){
