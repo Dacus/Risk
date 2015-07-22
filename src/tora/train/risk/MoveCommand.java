e9c9caa4aec2ec809ed734d4c2030fc7e0c918ba
Paul <paul.triscas@gmail.com>
2015-07-22 13:25:23 +0300

    Hot Seat Console Finished

--- src/tora/train/risk/MoveCommand.java
+++ src/tora/train/risk/MoveCommand.java
@@ -9,6 +9,7 @@ public class MoveCommand {
     private Point coordinates;
     private int numberOfUnits;
     private Player owner;
+    String direction;
 
     public int getNumberOfUnits() {
         return numberOfUnits;
@@ -19,14 +20,28 @@ public class MoveCommand {
     }
 
 
-    public MoveCommand(Point coordinates,int numberOfUnits, Player owner/*,Direction direction*/){
+    public MoveCommand(Point coordinates,int numberOfUnits, Player owner,String direction){
         this.coordinates=coordinates;
         this.numberOfUnits=numberOfUnits;
         this.owner=owner;
-       // this.direction=direction;
+        this.direction=direction;
     }
 
     public Point getCoordinates() {
         return coordinates;
+    }
+    public Point getDestinationPoint(){
+        switch (direction){
+            case "up":
+                return new Point(coordinates.x+1,coordinates.y);
+            case "down":
+                return new Point(coordinates.x-1,coordinates.y);
+            case "left":
+                return new Point(coordinates.x,coordinates.y-1);
+            case "right":
+                return new Point(coordinates.x,coordinates.y+1);
+            default:
+                return null;
+        }
     }
 }
