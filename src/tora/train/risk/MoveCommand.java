d372e21acd8ebe991d20c8b4ed39204c4e77c522
Paul <paul.triscas@gmail.com>
2015-07-22 10:27:12 +0300

    Move Command in progress

--- src/tora/train/risk/MoveCommand.java
+++ src/tora/train/risk/MoveCommand.java
@@ -19,11 +19,11 @@ public class MoveCommand {
     }
 
 
-    public MoveCommand(Point coordinates,int numberOfUnits, Player owner,Direction direction){
+    public MoveCommand(Point coordinates,int numberOfUnits, Player owner/*,Direction direction*/){
         this.coordinates=coordinates;
         this.numberOfUnits=numberOfUnits;
         this.owner=owner;
-        this.direction=direction;
+       // this.direction=direction;
     }
 
     public Point getCoordinates() {
