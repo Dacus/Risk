5f5bb477585243ec91c374b1d320f8ed1569a208
andreademe <andrea.deme>
2015-07-22 19:18:08 -0700

    Remove MainServerViewInterface

--- src/tora/train/risk/clientserver/serverapp/MainServerViewInterface.java
+++
@@ -1,9 +0,0 @@
-package tora.train.risk.clientserver.serverapp;
-
-/**
- * Created by Andrea on 7/16/2015.
- */
-public interface MainServerViewInterface {
-    int WINDOW_X=500;
-    int WINDOW_Y=500;
-}
