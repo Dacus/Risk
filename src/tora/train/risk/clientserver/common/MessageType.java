121ec3ef8076f0fcc391c57b4909a23507a36494
andreademe <andrea.deme>
2015-07-22 19:35:19 -0700

    Rename MessageTag ro MessageType.

--- src/tora/train/risk/clientserver/common/MessageType.java
+++ src/tora/train/risk/clientserver/common/MessageType.java
@@ -3,7 +3,7 @@ package tora.train.risk.clientserver.common;
 /**
  * Type of message
  */
-public enum MessageTag {
+public enum MessageType {
 	USER, GLOBAL, IDENTITY, READY, START, CONNECT, STOP, PLAYER_DISCONNECTED, NEW_PLAYER_CONNECTED,
     RESTRICT_CLIENT, RESTRICT_CONNECTION, ONLINE_PLAYERS
 }
